b631369afba36695a9206ac343d0320489365f1b
ubuntu <amackzie@gmail.com>
2025-01-11 11:12:14 +0200

    add workflow build step

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://github.com/zessu/zessu.github.io",
-  base: "/",
   integrations: [mdx(), sitemap()],
 });
