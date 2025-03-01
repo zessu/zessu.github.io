54e9cae85576cdc60a2cb757703e8723872d2fbf
amackzie@gmail.com <amackzie@gmail.com>
2025-03-01 16:58:21 +0300

    log errors

--- db/seed.ts
+++ db/seed.ts
@@ -1,16 +1,20 @@
 import { getCollection } from "astro:content";
-import { db, Post, Comment } from "astro:db";
+import { db, Post } from "astro:db";
 import crypto from "crypto";
 
 export default async function () {
-  const posts = await getCollection("blog", ({ data }) => {
-    return import.meta.env.PROD ? data.draft !== true : true;
-  });
+  try {
+    const posts = await getCollection("blog", ({ data }) => {
+      return import.meta.env.PROD ? data.draft !== true : true;
+    });
 
-  const completedPosts = posts.map((post) => {
-    const hash = crypto.createHash("sha256").update(post.id).digest("hex");
-    return { id: hash, likes: 0, reads: 0 };
-  });
+    const completedPosts = posts.map((post) => {
+      const hash = crypto.createHash("sha256").update(post.id).digest("hex");
+      return { id: hash, likes: 0, reads: 0 };
+    });
 
-  await db.insert(Post).values(completedPosts).execute();
+    await db.insert(Post).values(completedPosts).execute();
+  } catch (error) {
+    console.error(error);
+  }
 }
