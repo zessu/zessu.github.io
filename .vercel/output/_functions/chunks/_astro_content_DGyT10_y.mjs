c5f523e7a726614f8d2e5435c493deb0c6c02278
ddrew <amackzie@gmail.com>
2025-12-17 15:05:30 +0300

    geist font update, monospace roboto headers and icons for next article - build

--- .vercel/output/_functions/chunks/_astro_content_DGyT10_y.mjs
+++ .vercel/output/_functions/chunks/_astro_content_DGyT10_y.mjs
@@ -66,7 +66,7 @@ class ImmutableDataStore {
    */
   static async fromModule() {
     try {
-      const data = await import('./_astro_data-layer-content_BYfv0W01.mjs');
+      const data = await import('./_astro_data-layer-content_rVbyDJtL.mjs');
       if (data.default instanceof Map) {
         return ImmutableDataStore.fromMap(data.default);
       }
--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_8_YBCK5o.mjs';
-import { manifest } from './manifest_D4zeBjvk.mjs';
+import { manifest } from './manifest_CYldub7D.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -27,7 +27,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "86a80a8a-2122-48fa-a4d1-bbfd70d0217f",
+    "middlewareSecret": "45ee8160-f5c1-4be8-bb5c-76abe83efaa1",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
