4e76c031009721389294aac4b513abd6eedb20ee
ddrew <amackzie@gmail.com>
2026-03-09 17:39:00 +0300

    remove vercel json

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_8_YBCK5o.mjs';
-import { manifest } from './manifest_DNSHvZxe.mjs';
+import { manifest } from './manifest_D5t5h1Kl.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -27,7 +27,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "68c6813f-cc91-48a2-89a7-636fd9b40e0c",
+    "middlewareSecret": "c7e0bdad-2ee4-4009-81b9-3073e12a525f",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
